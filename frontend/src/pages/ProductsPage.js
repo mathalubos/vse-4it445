5788ad5538fcfa9f29047e777d521a33f80b1beb
Maťha Luboš <matha.lubos@gmail.com>
2016-10-04 19:28:03 +0200

    loading data from rest api and processing via setStatus

--- frontend/src/pages/ProductsPage.js
+++ frontend/src/pages/ProductsPage.js
@@ -4,44 +4,24 @@ import axios from 'axios';
 import { ProductList } from '../components/ProductList/ProductList.js';
 
 export class ProductsPage extends Component {
+  constructor(props){
+    super(props);
+    this.state = {'data':[]};
+  }
 
   componentDidMount() {
     axios('http://dev.backend.xmatl41.vse.handson.pro/api/products')
     .then((response) => {
-      this.setState({'response' : response});
+      this.setState(response);
     });
   }
   render() {
-    if (this.state.data) {
-      console.log(this.state.response);
-    }
-
-    const products = [
-      {
-        id: 1,
-        title: 'Škoda Superb',
-        price: '750 000',
-        shortInfo: 'Luxury car produced in the Czech Republic.',
-      },
-      {
-        id: 2,
-        title: 'Ford Focus',
-        price: '600 000',
-        shortInfo: 'Sports car made in USA.',
-      },
-      {
-        id: 3,
-        title: 'Moped',
-        price: '1 000',
-        shortInfo: 'No comment.',
-      },
-    ]
     return (
       <div>
         <div className="jumbotron">
           <h1>All Products</h1>
         </div>
-        <ProductList products={products}/>
+        <ProductList products={this.state.data}/>
       </div>
     );
   }
