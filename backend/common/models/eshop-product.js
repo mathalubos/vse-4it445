8878c3510f3ce160e149d9f55ca8c269f894e441
Maťha Luboš <matha.lubos@gmail.com>
2016-10-11 19:32:51 +0200

    added new model category; created relation to product and created calling on api to list products with categories

--- backend/common/models/eshop-product.js
+++ backend/common/models/eshop-product.js
@@ -1,5 +1,21 @@
 'use strict';
 
-module.exports = function(Eshopproduct) {
+module.exports = function(EshopProduct) {
+  EshopProduct.withCategory = function(callback){
+    EshopProduct.find({
+        where: {
+        },
+        include: 'category'
+    }, callback);
+  }
 
+  EshopProduct.remoteMethod(
+    'withCategory',
+    {
+      http: {
+        verb: 'get',
+      },
+      returns: {arg:'products', type:'array'},
+    }
+  );
 };
--- backend/server/boot/create-sample-products.js
+++ backend/server/boot/create-sample-products.js
@@ -1,26 +1,51 @@
 module.exports = function(app) {
-  const { EshopProduct } = app.models;
-  if (!EshopProduct) { return; }
+  app.dataSources.mysqlds.autoupdate('EshopProduct', function(err) {
+    const { EshopProduct } = app.models;
+    if (!EshopProduct) { return; }
 
-  EshopProduct.create([
-      {
-        title: 'Škoda Superb',
-        price: 750000,
-        shortInfo: 'Luxury car produced in the Czech Republic.',
-      },
-      {
-        title: 'Ford Focus',
-        price: 600000,
-        shortInfo: 'Sports car made in USA.',
-      },
-      {
-        title: 'Moped',
-        price: 1000,
-        shortInfo: 'No comment.',
-      },
-  ], function(err, products) {
-    if (err) throw err;
+    EshopProduct.create([
+        {
+          title: 'Škoda Superb',
+          price: 750000,
+          shortInfo: 'Luxury car produced in the Czech Republic.',
+          categoryId: 1,
+        },
+        {
+          title: 'Ford Focus',
+          price: 600000,
+          shortInfo: 'Sports car made in USA.',
+          categoryId: 2,
+        },
+        {
+          title: 'Moped',
+          price: 1000,
+          shortInfo: 'No comment.',
+          categoryId: 5,
+        },
+    ], function(err, products) {
+      if (err) throw err;
 
-    console.log('Models created: \n', products);
+      console.log('Models created: \n', products);
+    });
   });
+  // app.dataSources.mysqlds.autoupdate('Category', function(err) {
+  //   const { Category } = app.models;
+  //   if (!Category) { return; }
+  //
+  //   Category.create([
+  //       {
+  //         name: 'category 1',
+  //       },
+  //       {
+  //         name: 'category 2',
+  //       },
+  //       {
+  //         name: 'category 3',
+  //       },
+  //   ], function(err, products) {
+  //     if (err) throw err;
+  //
+  //     console.log('Models created: \n', products);
+  //   });
+  // });
 };
