e623567f8f5348560ff3d87da7f9b12917d30eae
Maťha Luboš <matha.lubos@gmail.com>
2016-10-25 19:38:24 +0200

    added cart with form for sending order information to backend

--- frontend/src/pages/ProductsPage.js
+++ frontend/src/pages/ProductsPage.js
@@ -3,19 +3,38 @@ import React, { Component } from 'react';
 import api from '../api.js';
 import { ProductList } from '../components/ProductList/ProductList.js';
 
+import lodash from 'lodash';
+
 export class ProductsPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       products: null,
     };
+    this.fetchProductsDebounces = lodash.debounce(this.fetchProducts, 500);
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+  }
+  handleSearchChange(event) {
+    const searchString = event.target.value;
+    this.fetchProductsDebounces(searchString);
   }
 
-  componentDidMount() {
-    api('products')
+  paramsForSearchString(searchString) {
+    if (!searchString) {
+      return {};
+    }
+    return {filter:{where:{title:{like:`%${searchString}%`}}}}
+  }
+
+  fetchProducts(searchString) {
+    api('products', {params: this.paramsForSearchString(searchString)})
       .then((response) => {
         this.setState({ products: response.data });
       });
+  }
+
+  componentDidMount() {
+    this.fetchProducts();
   }
 
   render() {
@@ -23,6 +42,10 @@ export class ProductsPage extends Component {
 
     return (
       <div>
+      <div>
+        <label>Search:</label>
+        <input type="text" onChange={this.handleSearchChange}/>
+      </div>
         <div className="jumbotron">
           <h1>All Products</h1>
         </div>
--- frontend/src/pages/ShoppingCartCheckoutPage.js
+++ frontend/src/pages/ShoppingCartCheckoutPage.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import api from '../api.js';
 
-export class ShoppingCartCheckoutPage extends Component {
+import { connect } from 'react-redux';
+
+export class ShoppingCartCheckoutPageRaw extends Component {
   constructor(props) {
     super(props);
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {firstName : ""};
   }
 
   handleSubmit(event) {
+    event.preventDefault();
+    const {items} = this.props;
+    const formItems = items.map(({product, quantity}) => ({
+      productId:product.id,
+      quantity,
+    }));
+    const formData = new FormData(event.target);
+    formData.append(
+      'items',
+      JSON.stringify(
+        formItems
+      )
+    );
+    api.post('orders/submit', formData)
+      .then(({data}) => console.log('data', data));
   }
 
   render() {
@@ -19,11 +38,19 @@ export class ShoppingCartCheckoutPage extends Component {
         <form onSubmit={this.handleSubmit}>
           <div>
             <div><label htmlFor="firstName">First Name</label></div>
-            <input type="text" name="firstName" id="firstName" />
+            <input
+            value={this.state.firstName}
+            onChange={event => this.setState({
+              firstName:event.target.value.slice(0,5)
+            })}
+            type="text"
+            name="firstName"
+            id="firstName"
+            />
           </div>
           <div>
             <div><label htmlFor="lastName">Last Name</label></div>
-            <input type="text" name="lastName" id="lastName" />
+            <input defaultValue="test" type="text" name="lastName" id="lastName" />
           </div>
           <div>
             <div><label htmlFor="address">Address</label></div>
@@ -37,3 +64,16 @@ export class ShoppingCartCheckoutPage extends Component {
     );
   }
 }
+import {getItems} from '../components/ShoppingCart/reducer.js';
+
+function mapStateToProps(state) {
+  const {shoppingCart} = state;
+  return {
+    items: getItems (shoppingCart),
+  };
+}
+export const ShoppingCartCheckoutPage = connect(
+  mapStateToProps,
+  {
+  }
+)(ShoppingCartCheckoutPageRaw);
